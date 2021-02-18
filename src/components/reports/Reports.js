489b626967df7a115646de8deaa8063c60318526
Usuario Apiux <nadia.gallardo@apiux.com>
2021-02-18 10:43:02 -0300

    cambios reporteria

--- src/components/reports/Reports.js
+++ src/components/reports/Reports.js
@@ -7,7 +7,44 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import IntlMessages from 'util/IntlMessages';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import PropTypes from 'prop-types';
+import SearchMissingDoc from './ui/SearchMissingDoc.js';
 
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+  
+	return (
+	  <div
+		role="tabpanel"
+		hidden={value !== index}
+		id={`simple-tabpanel-${index}`}
+		aria-labelledby={`simple-tab-${index}`}
+		{...other}
+	  >
+		{value === index && (
+		  <Box p={3}>
+			<Typography>{children}</Typography>
+		  </Box>
+		)}
+	  </div>
+	);
+  }
+  
+  TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.any.isRequired,
+	value: PropTypes.any.isRequired,
+  };
+  
+  function a11yProps(index) {
+	return {
+	  id: `simple-tab-${index}`,
+	  'aria-controls': `simple-tabpanel-${index}`,
+	};
+  }
 
 const useStyles = makeStyles({
 	root: {
@@ -47,6 +84,7 @@ const Reports = () => {
 							</h3>
 						</div>
 			<div className={classes.root}>
+			
 				<Tabs
 					value={value}
 					onChange={handleChange}
@@ -54,10 +92,20 @@ const Reports = () => {
 					textColor="primary"
 					centered
 				>
-					<Tab label="Item One" />
-					<Tab label="Item Two" />
-					<Tab label="Item Three" />
+					<Tab label="Carga Masiva" {...a11yProps(0)}  />
+					<Tab label="Documentos Faltantes" {...a11yProps(1)} />
+					<Tab label="Item Three" {...a11yProps(2)} />
 				</Tabs>
+				
+				<TabPanel value={value} index={0}>
+					<DateSearchReports/>
+				</TabPanel>
+				<TabPanel value={value} index={1}>
+					<SearchMissingDoc/>
+				</TabPanel>
+				<TabPanel value={value} index={2}>
+					Item Three
+				</TabPanel>
 			</div>
 		</div>
 		</div>
