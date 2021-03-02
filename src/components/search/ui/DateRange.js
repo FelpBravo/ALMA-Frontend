86066c54c90798b19b889e95c51ca0b52a5c47da
Usuario Apiux <nadia.gallardo@apiux.com>
2021-03-02 15:34:06 -0300

    nuevo componente daterange

--- src/components/search/ui/DateRange.js
+++ src/components/search/ui/DateRange.js
@@ -1,26 +1,48 @@
 import React, { useState, Text} from 'react';
-import { TextField, Grid  } from '@material-ui/core';
+import { TextField, Grid, makeStyles  } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 
 import IntlMessages from 'util/IntlMessages';
 
-
+const useStyles = makeStyles((theme) => ({
+	root: {
+        border: "1px solid #a1a1a1",
+        borderRadius: 4,
+        padding: 2,
+        height: 50,
+        width: 520,
+        
+    },
+    text:{
+        width: 100,
+        fontSize: 12,
+        marginTop: -12,
+        marginLeft: 12,
+        background: "white",
+    },
+    inter:{
+        padding:4,
+        marginTop:-15,
+        marginLeft: 15,
+    }	
+  }));
 
 export const DateRange = () => {
 
-
+	const classes = useStyles();
 
 	return (
+        <div className={classes.root}>
         <div>
-     
-		<Grid container spacing={2}>
+         <p className={classes.text}>Modificado en</p>
+        </div>
+		<Grid container className={classes.inter}>
 			<Grid item xs={4}>
             <TextField
 					//key={name}
 					//label={label}
 					//name={name}
-					variant="outlined"
 					fullWidth
 					type="date"
 					//value={value ? value : '2017-05-24'}
@@ -33,12 +55,14 @@ export const DateRange = () => {
 					//onChange={handleOnChange}
 				/>
             </Grid>
+            <Grid  className="ml-5" item xs={2} >
+            <p>Hasta</p>
+            </Grid>
             <Grid item xs={4}>
 			<TextField
 					//key={name}
 					//label={label}
 					//name={name}
-					variant="outlined"
 					fullWidth
 					type="date"
 					//value={value ? value : '2017-05-24'}
