2e9470119c90ef5e25bbe476e3d42638242e42e0
Usuario Apiux <nadia.gallardo@apiux.com>
2021-03-17 18:01:43 -0300

    creacion div de comentarios existentes

--- src/components/search/ui/Information/ui/Comments.js
+++ src/components/search/ui/Information/ui/Comments.js
@@ -36,16 +36,15 @@ const Comments = () =>{
             <Grid container>
             <Grid item xs={10}>
                <TextField
-                    id="outlined-secondary"
                     label="Escribe nuevo comentario"
                     variant="outlined"
                     color="primary"
                     fullWidth
                 />
             </Grid>
-            <Grid item xs={2} style={{padding:4}}>
-                <IconButton type="submit" style={{background: "#3699FF"}}>
-                    <NearMeOutlinedIcon style={{color: "white"}} />
+            <Grid item xs={1} className="mt-3 ml-1">
+                <IconButton type="submit"  style={{background: "#3699FF", width: 15, height: 15}}>
+                    <NearMeOutlinedIcon style={{color: "white", fontSize: 15}} />
                 </IconButton>
             </Grid>   
             </Grid>
@@ -53,7 +52,7 @@ const Comments = () =>{
                     <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
                     <label htmlFor="icon-button-file">
                         <IconButton color="primary" aria-label="upload picture" component="span">
-                        <AttachFileOutlinedIcon />
+                        <AttachFileOutlinedIcon fontSize="small"/>
                         </IconButton>
                     </label>
 					<p style={{ fontFamily: "Poppins", fontSize: '12px', fontWeight: 400, color: "#3699FF", marginTop:13}}	>
@@ -69,13 +68,14 @@ const Comments = () =>{
        <NewComennt/>
        <div className={classes.coment}>
            <span style={{color:"#3699FF"}}>Nadia Gallardo</span>
-           <p style={{fontSize: 13}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.</p>
-           <Grid container>
-            <span style={{color:"#3699FF", fontSize: 11}}>Nombre documento adjunto</span>
-           </Grid>
-        <Grid container style={{fontSize: 14}}>
-            <MessageOutlinedIcon fontSize="small" />
-            <span>Comentar</span>
+           <p style={{fontSize: 13}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.</p>    
+            <p style={{color:"#3699FF", fontSize: 11}}>Nombre documento adjunto</p>
+        
+        <Grid container >
+            <MessageOutlinedIcon style={{fontSize: 20}}/>
+            <span style={{fontSize: 12}} >Comentar</span>
+
+            <span style={{marginLeft: 5, fontSize: 12, color:"#A7A8BB"}} >2 comentarios</span>
         </Grid>
         
        </div>
