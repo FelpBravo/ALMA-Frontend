3cb8d2e45df3d02b2c55098198cd8f361eebf2c9
JoseGuerreroT <joseeguerreroot@gmail.com>
2021-04-05 11:45:27 -0500

    Idle timer with validations for tabs

--- src/routers/MainRouter.js
+++ src/routers/MainRouter.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router'
 
 import AppRouter from './AppRouter';
 import { history } from '../store/store';
+import IdleTimer from 'helpers/IdleTimer';
+import { useDispatch, useSelector } from 'react-redux';
+import { startUserSingOut } from 'actions/auth';
+
+const TIMEOUT = 60*0.5; // HardCode for test
 
 export const MainRouter = () => {
+	const { authUser } = useSelector(state => state.auth);
+	const dispatch = useDispatch()
+
+	const clearToken = () => dispatch(startUserSingOut());
+
+	useEffect(() => {
+		if (authUser) {
+			const timer = new IdleTimer({
+				timeout: TIMEOUT, // In seconds
+				onTimeout: () => clearToken(),
+				onExpired: () => clearToken(), //do something if expired on load
+			});
+			return () => {
+				timer.cleanUp();
+			};
+		}
+
+	}, [authUser]);
+
 	return (
 		<ConnectedRouter history={history}>
 			<Switch>
