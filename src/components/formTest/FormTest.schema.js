31fe85be37b72b32f433ace1d3fb2d9894e07203
Jose Guerrero <dev.joseguerrero@gmail.com>
2021-05-04 17:09:08 -0500

    Clean Code

--- src/components/formTest/FormTest.schema.js
+++ src/components/formTest/FormTest.schema.js
@@ -1,11 +1,11 @@
 import { object, string, number, date } from 'yup';
 
 const schema = object().shape({
-    // title: string().min(3, "Minimo 3 caracteres").required("Titulo Campo requerido"),
-    // email: string().email().required("Titulo Campo requerido"),
-    // goal: number().positive(""),
-    // status: string().required(),
-    // dueDate: date().required().min("2021-12-01")
+    title: string().min(3, "Minimo 3 caracteres").required("Titulo Campo requerido"),
+    email: string().email().required("Titulo Campo requerido"),
+    goal: number().positive(""),
+    status: string().required(),
+    dueDate: date().required().min("2021-12-01")
 });
 
 export default schema;
