5c45cdf8c1a69107f98eac97fd11d6618c27d4e3
Jose Guerrero <dev.joseguerrero@gmail.com>
2021-05-18 19:36:47 -0500

    Fieldarray complex structure

--- src/components/documents/steps/request.js
+++ src/components/documents/steps/request.js
@@ -1,33 +1,112 @@
-import { Grid, makeStyles, Paper } from '@material-ui/core';
-import React from 'react';
+import { Divider, Grid, IconButton, makeStyles, Paper } from '@material-ui/core';
+import React, { useEffect } from 'react';
 
 import { TitleCard } from 'components/ui/helpers/TitleCard';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { countries } from 'components/formTest/countries';
 import { AutoCompleteField } from 'components/ui/Form';
 import RolesData from './roles.json';
+import Button from 'components/ui/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
-
+    rolTitle: {
+        margin: theme.spacing(0, 1),
+    },
+    rolSubTitle: {
+        color: "gray",
+        margin: theme.spacing(0, 1),
+    }
 }));
+
+const NestedArray = ({ name, control, commonProps, rolName, index, setValue, mandatory }) => {
+    const { fields, append, remove } = useFieldArray({ name, control });
+    const classes = useStyles();
+
+    useEffect(() => {
+        setValue(`approves[${index}].role`, rolName);
+    }, [])
+
+    const addField = e => {
+        e.preventDefault()
+        append({})
+    }
+
+    return (<Grid item md={12} spacing={3} container alignItems="center">
+        <Grid item md={12} spacing={2} container alignItems="center">
+            <h5 className={classes.rolTitle}>{rolName}</h5>
+            <p className={classes.rolSubTitle}>{mandatory ? "(Requerido)" : "(Opcional)"}</p>
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={addField}>
+                Agregar
+            </Button>
+        </Grid>
+        <Grid item md={12} container alignItems="center">
+            {
+                fields.map((field, index) => (
+                    <Grid item md={12} container spacing={4} key={field}>
+                        <Grid item md>
+                            <AutoCompleteField
+                                name={`${name}[${index}].name`}
+                                label="Seleccionar nombre"
+                                options={countries}
+                                optionsLabel="label"
+                                optionsValue="label"
+                                {...commonProps} />
+                        </Grid>
+                        <Grid item md>
+                            <AutoCompleteField
+                                name={`${name}[${index}].lastname`}
+                                label="Seleccionar nombre"
+                                options={countries}
+                                optionsLabel="label"
+                                optionsValue="label"
+                                {...commonProps} />
+                        </Grid>
+                        <Grid item md>
+                            <AutoCompleteField
+                                name={`${name}[${index}].age`}
+                                label="Seleccionar nombre"
+                                options={countries}
+                                optionsLabel="label"
+                                optionsValue="label"
+                                {...commonProps} />
+                        </Grid>
+                        <Grid item md={1}>
+
+                            <IconButton onClick={() => remove(index)} aria-label="delete">
+                                <DeleteIcon fontSize="small" />
+                            </IconButton>
+                        </Grid>
+
+                    </Grid>
+                ))
+            }
+        </Grid>
+        <Grid item md={12} alignItems="center">
+            <Divider />
+        </Grid>
+    </Grid>)
+}
 
 export default function RequestStep() {
     const classes = useStyles();
-    const { control, register, handleSubmit, formState: { errors } } = useForm();
-    const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
-        control, // control props comes from useForm (optional: if you are using FormContext)
-        name: "approves", // unique name for your Field Array
-        // keyName: "id", default to "id", you can change the key name
+    const { control, register, handleSubmit, formState: { errors }, setValue } = useForm({
+        defaultValues: {}
+    });
+
+    const { fields } = useFieldArray({
+        control,
+        name: "approves",
     });
 
     const onSubmit = values => {
         console.log("values", values)
         alert(JSON.stringify(values))
     };
-    const addField = e => {
-        e.preventDefault()
-        append({})
-    }
+
 
     const commonProps = {
         register,
@@ -36,40 +115,25 @@ export default function RequestStep() {
         shrink: true,
         size: "small",
     }
-    console.log("fields", fields)
 
     return <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={2}>
             <Grid item md={12}>
                 <TitleCard message="document.title.requestDocument" />
             </Grid>
+
             {
-                RolesData.map(({ name }, rolId) => <Grid item md={12} container spacing={2}>
-                    <>
-                        <h6>{name}</h6>
-                        <button onClick={addField}>Agregar</button>
-                        {
-                            fields.map((field, index) => (
-                                <Grid item md={12} container key={field} spacing={2}>
-                                    <Grid item md>
-                                        <AutoCompleteField
-                                            name={`approves.${rolId}.users.${index}`}
-                                            label="Seleccionar nombre"
-                                            options={countries}
-                                            optionsLabel="label"
-                                            optionsValue="label"
-                                            {...commonProps} />
-                                    </Grid>
-                                    {/* <input
-                            key={field.id} // important to include key with field's id
-                            {...register(`name.${index}`)}
-                            defaultValue={field.value} // make sure to include defaultValue
-                        /> */}
-                                </Grid>
-                            ))
-                        }
-                    </>
-                </Grid>)
+                RolesData.map(({ name, ...rest }, index) =>
+                    <Grid container item md={12} spacing={2}>
+                        <NestedArray
+                            index={index}
+                            rolName={name}
+                            setValue={setValue}
+                            name={`approves[${index}].users`}
+                            commonProps={commonProps}
+                            control={control}
+                            {...rest} />
+                    </Grid>)
             }
 
             <Grid container item md={12}>
