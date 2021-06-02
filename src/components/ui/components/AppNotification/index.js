8f64ce212bca6ee7575c00a85435e1a46c6b569a
Jose Guerrero <dev.joseguerrero@gmail.com>
2021-06-02 11:45:44 -0500

    Stop condition

--- src/components/ui/components/AppNotification/index.js
+++ src/components/ui/components/AppNotification/index.js
@@ -1,25 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
-
-import NotificationItem from './NotificationItem';
-import { notifications } from './data';
-import CustomScrollbars from 'util/CustomScrollbars';
 import { CircularProgress, Grid } from '@material-ui/core';
-import { startNotificationsLoading } from 'actions/notifications';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+import { startNotificationsLoading } from 'actions/notifications';
+import CustomScrollbars from 'util/CustomScrollbars';
+
+import { notifications } from './data';
+import NotificationItem from './NotificationItem';
+
+const SIZE = 5;
 
 const AppNotification = ({ isOpen = false }) => {
   const dispatch = useDispatch();
   const { authUser } = useSelector(state => state.auth);
-  const { currentPage, totalItems, totalPages, data } = useSelector(state => state.notifications);
-
+  const { currentPage, totalItems, data } = useSelector(state => state.notifications);
+  const totalPages = Math.ceil(totalItems / SIZE);
 
   const loader = useRef(null);
   const [page, setPage] = useState(currentPage)
-  
+  const isFinishLoad = totalPages === 0 ? false : totalPages === page ? true : false
+
   const handleObserver = (entities) => {
     const target = entities[0];
     if (target.isIntersecting) {
-      console.log("setPage", page + 1)
       setPage((page) => page + 1)
     }
   }
@@ -39,21 +42,21 @@ const AppNotification = ({ isOpen = false }) => {
   }, [isOpen]);
 
   useEffect(() => {
-    console.log("authUser", authUser, "page", page)
     if(page > 0)
-      dispatch(startNotificationsLoading({authUser, page, size:5}))
-    // const newList = data.concat(notifications);
-    // setTimeout(() => setData([...newList]), 2000)
+      dispatch(startNotificationsLoading({ authUser, page, size: SIZE}))
   }, [authUser,page])
-
+  console.log("Termino", isFinishLoad)
   return (
     <CustomScrollbars className="messages-list scrollbar" style={{ height: 380 }}>
       <ul className="list-unstyled">
         {data.map((notification, index) => <NotificationItem key={index} notification={notification} />)
         }
+        {
+        !isFinishLoad &&
         <Grid container alignItems="center" justify="center" ref={loader}>
           <CircularProgress size={30} thickness={7} />
         </Grid>
+        }
       </ul>
     </CustomScrollbars>
   )
