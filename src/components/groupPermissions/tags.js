c9d5a68774c52e56d918f5e1b1cf7b03364817d4
Jose Guerrero <dev.joseguerrero@gmail.com>
2021-06-21 12:24:54 -0500

    Fix responsive profile view

--- src/components/groupPermissions/tags.js
+++ src/components/groupPermissions/tags.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import MenuItem from '@material-ui/core/MenuItem';
+import { Grid, InputLabel } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from '@material-ui/core/FormControl';
 import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Grid, InputLabel } from '@material-ui/core';
-import Chip from 'components/ui/Chip';
 import { get } from 'lodash-es';
+import React from 'react';
+
+import Chip from 'components/ui/Chip';
 
 const Tags = ({ moduleId, tagsList, name, updateActions, tagsSelected}) => {
     const handleChange = ({ target }) => {
@@ -27,7 +28,7 @@ const Tags = ({ moduleId, tagsList, name, updateActions, tagsSelected}) => {
     const getNameById = id => get(tagsList.find(e =>e.id === id), 'name', '')
 
     return (<Grid container spacing={1}>
-        <Grid item md={3}>
+        <Grid item md={3} xs={10} sm={10}>
             <FormControl fullWidth variant="outlined" size="small" >
                 <InputLabel id="demo-mutiple-checkbox-label">{name}</InputLabel>
                 <Select
@@ -58,7 +59,7 @@ const Tags = ({ moduleId, tagsList, name, updateActions, tagsSelected}) => {
                 </Select>
             </FormControl>
         </Grid>
-        <Grid item md={9} container>
+        <Grid item md={9} xs={12} sm={12} container>
             {tagsSelected?.map( id => <Chip
                 key={`${getNameById(id)}${id}${Math.random()}`}
                 size="small"
