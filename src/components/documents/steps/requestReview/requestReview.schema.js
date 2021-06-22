e450514cb0eeb253186dc63ef761c95e6391c742
Jose Guerrero <dev.joseguerrero@gmail.com>
2021-06-22 18:23:19 -0500

    rol user unique

--- src/components/documents/steps/requestReview/requestReview.schema.js
+++ src/components/documents/steps/requestReview/requestReview.schema.js
@@ -1,11 +1,5 @@
 import * as yup from 'yup';
 
-yup.addMethod(yup.array, 'unique', function (message, mapper = a => a) {
-    return this.test('unique', message, function (list) {
-        return list.length === new Set(list.map(mapper)).size;
-    });
-});
-
 const schema = yup.object().shape({
     approves: yup.array().of(
         yup.object().shape({
@@ -15,7 +9,7 @@ const schema = yup.object().shape({
                     userId: yup.string().required(),
                     maxDays: yup.number().required().positive().max(99).integer(),
                 })
-            ).unique('duplicate phone', a => a.userId)
+            )
         })
     )
 });
