2a9dbd94391c78361dc5ba8148e805b0a3cf9909
Jose Guerrero <dev.joseguerrero@gmail.com>
2021-06-22 09:08:54 -0500

    Validation repeat

--- src/components/documents/steps/requestReview/requestReview.schema.js
+++ src/components/documents/steps/requestReview/requestReview.schema.js
@@ -1,15 +1,21 @@
-import { array, number, object, string } from 'yup';
+import * as yup from 'yup';
 
-const schema = object().shape({
-    approves: array().of(
-        object().shape({
-            role: string().required(),
-            users: array().of(
-                object().shape({
-                    userId: string().required(),
-                    maxDays: number().required().positive().max(99).integer(),
+yup.addMethod(yup.array, 'unique', function (message, mapper = a => a) {
+    return this.test('unique', message, function (list) {
+        return list.length === new Set(list.map(mapper)).size;
+    });
+});
+
+const schema = yup.object().shape({
+    approves: yup.array().of(
+        yup.object().shape({
+            role: yup.string().required(),
+            users: yup.array().of(
+                yup.object().shape({
+                    userId: yup.string().required(),
+                    maxDays: yup.number().required().positive().max(99).integer(),
                 })
-            )
+            ).unique('duplicate phone', a => a.userId)
         })
     )
 });
